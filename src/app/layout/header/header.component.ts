8f5e05ca70a5a4f2d6f869f2083f73b7560bb03c
sameerahex <sameer.rout@ahex.co.in>
2023-02-02 12:29:51 +0530

    add void in function

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as mapboxgl from 'mapbox-gl';
-import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
